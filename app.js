13d410f699e61a029d98735476d4f70a154ebce9
Yoshimasa Sakamoto <yoshi.sakamoto@nifty.ne.jp>
2020-09-11 10:50:28 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 const memo = new Map();
-memo.set(0,0);
-memo.set(1,1);
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
     if (memo.has(n)) {
         return memo.get(n);
     }
-    const value = fib(n -1) + fib(n -2);
-    memo.set(n, value);
-    return value;
+    const value = fib(n-1) + fib(n - 2);
+    memo.set(n, value)
+    return value;   
 }
-const length = 40;
+const length = 1000;
 for (let i = 0; i <= length; i++) {
-    console.log(fib(i));
+    console.log(i + "のフィボナッチ数は" + fib(i) + "です。");
 }
